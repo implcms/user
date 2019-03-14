1a6de333869f7e732cec0b11bab188e1f8b7997d
sayiwen <sayiwen@163.com>
2019-03-14 17:42:26 +0800

    init user

---
+++ controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php namespace Applications\User\Controllers;
+
+
+/**
+ * 用户控制器
+ */
+class UserController{
+
+    /**
+     * 登录中间件
+     */
+    
+    public function apiAuth($param){
+		
+    }
+
+}
